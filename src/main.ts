f21a00e537886a86903ac207533872fe38935795
yuweiZhan <195812208@qq.com>
2020-11-21 14:10:14 +0800

    :sparkles: vuex

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,23 @@ import App from './App.vue'
 import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '@/views/Home.vue'
 import Login from '@/views/Login.vue'
-import Column from '@/views/Column.vue'
 import ColumnDetail from '@/views/ColumnDetail.vue'
+import { createStore } from 'vuex'
 
+const store = createStore({
+  state: {
+    count: 0
+  },
+  mutations: {
+    add (state) {
+      state.count++
+    }
+  }
+})
+
+console.log('store', store.state.count)
+store.commit('add')
+console.log('store', store.state.count)
 const routerHistory = createWebHashHistory()
 const router = createRouter({
   history: routerHistory,
